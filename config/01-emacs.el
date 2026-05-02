144ee6e458c34a092e03558d53a0bef7dd833539
shian2fp <shian2fp@users.noreply.github.com>
2026-05-02 23:12:00 +0900

    Add config files.

---
+++ config/01-emacs.el
@@ -0,0 +1,14 @@
+;;;
+;;; ~/.emacs.d/config/01-emacs.el
+;;;
+
+;; do not make backup files.
+(setq make-backup-files nil)
+
+;; move the custom file.
+(setq custom-file (locate-user-emacs-file "config/00-custom.el"))
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8-unix
+;;; End:
---
+++ config/02-package.el
@@ -0,0 +1,15 @@
+;;;
+;;; ~/.emacs.d/config/02-package.el
+;;;
+
+(require 'package)
+
+(push '(melpa . "https://melpa.org/packages/")
+      package-archives)
+
+(package-initialize)
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8-unix
+;;; End:
