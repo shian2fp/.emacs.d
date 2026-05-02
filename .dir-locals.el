c14fc7372f521e0a08d443d3181b58cb544881f6
shian2fp <shian2fp@users.noreply.github.com>
2026-05-02 18:09:41 +0900

    Add directory local variables.

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+;;;
+;;; ~/.emacs.d/.dir-locals.el
+;;;
+
+((nil . ((fill-column . 74)
+         (indent-tabs-mode . nil))))
+
+;;; Local Variables:
+;;; mode: lisp-data
+;;; coding: utf-8-unix
+;;; End:
