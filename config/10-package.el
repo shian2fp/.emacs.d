39ac052bd5dbfe666b56e7f8242df330d504ee9b
shian2fp <shian2fp@users.noreply.github.com>
2026-05-04 15:04:30 +0900

    No longer call `package-initialize' while initialization.

--- config/10-package.el
+++ config/10-package.el
@@ -2,12 +2,8 @@
 ;;; ~/.emacs.d/config/10-package.el
 ;;;
 
-(require 'package)
-
 (push '(melpa . "https://melpa.org/packages/")
       package-archives)
-
-(package-initialize)
 
 ;;; Local Variables:
 ;;; mode: emacs-lisp
