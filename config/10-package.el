bc1fdd4cb1aab4f093d73b58e2fab4e2021e8116
shian2fp <shian2fp@users.noreply.github.com>
2026-05-04 15:40:04 +0900

    Add a helper function to install a package.

--- config/10-package.el
+++ config/10-package.el
@@ -6,6 +6,14 @@
 (push '("melpa" . "https://melpa.org/packages/")
       package-archives)
 
+(defun init--ensure-package-exist (package &optional autoloads)
+  (unless (package-installed-p package)
+    (unless package-archive-contents
+      (package-refresh-contents))
+    (package-install package)
+    (when autoloads
+      (require autoloads))))
+
 ;;; Local Variables:
 ;;; mode: emacs-lisp
 ;;; coding: utf-8-unix
