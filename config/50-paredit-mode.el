018719b8968d8ea5248ffb4140f55a584611b8f5
shian2fp <shian2fp@users.noreply.github.com>
2026-05-04 17:15:18 +0900

    Reduce use of `with-eval-after-load' in `50-paredit-mode.el'.

--- config/50-paredit-mode.el
+++ config/50-paredit-mode.el
@@ -4,13 +4,11 @@
 
 (init--ensure-package-exist 'paredit)
 
-(with-eval-after-load 'lisp-mode
-  (push 'paredit-mode lisp-data-mode-hook)
-  (push 'paredit-mode lisp-mode-hook))
+(push 'paredit-mode lisp-data-mode-hook)
+(push 'paredit-mode lisp-mode-hook)
 
-(with-eval-after-load 'elisp-mode
-  (push 'paredit-mode emacs-lisp-mode-hook)
-  (push 'paredit-mode lisp-interaction-mode-hook))
+(push 'paredit-mode emacs-lisp-mode-hook)
+(push 'paredit-mode lisp-interaction-mode-hook)
 
 (with-eval-after-load 'scheme
   (push 'paredit-mode scheme-mode-hook))
