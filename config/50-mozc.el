3fb6caa6bc9786be9574680c9bb630d0029fb123
shian2fp <shian2fp@users.noreply.github.com>
2026-05-04 16:09:11 +0900

    Add mozc config file.

---
+++ config/50-mozc.el
@@ -0,0 +1,12 @@
+;;;
+;;; ~/.emacs.d/config/50-mozc.el
+;;;
+
+(init--ensure-package-exist 'mozc)
+
+(setq default-input-method "japanese-mozc")
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8-unix
+;;; End:
