7f72870920297ebb09040229e536217d4b52c205
shian2fp <shian2fp@users.noreply.github.com>
2026-07-13 22:30:49 +0900

    Reformat config/30-lisp-mode.el

--- config/30-lisp-mode.el
+++ config/30-lisp-mode.el
@@ -3,14 +3,13 @@
 ;;;
 
 (with-eval-after-load 'cl-indent
-  (dolist (pair
-           '((defsystem . (4 &rest 2))
-             (define-system . (4 &rest 2))
-             (destructuring-bind . ((&whole 6 &rest 1) 2 &body))
-             (multiple-value-bind . ((&whole 6 &rest 1) 2 &body))
-             (multiple-value-prog1 . 0)
-             (prog1 . 0)
-             (prog2 . 0)))
+  (dolist (pair '((defsystem . (4 &rest 2))
+                  (define-system . (4 &rest 2))
+                  (destructuring-bind . ((&whole 6 &rest 1) 2 &body))
+                  (multiple-value-bind . ((&whole 6 &rest 1) 2 &body))
+                  (multiple-value-prog1 . 0)
+                  (prog1 . 0)
+                  (prog2 . 0)))
     (put (car pair) 'common-lisp-indent-function (cdr pair))))
 
 ;;; Local Variables:
